18ff1607948fd64d598c1b1984ce0f19cbaabb87
masud2005 <masud20012005@gmail.com>
2026-01-23 11:00:06 +0600

    setup CD and config all everything

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { success: boolean; statusCode: number; message: string } {
+    return {
+      success: true,
+      statusCode: 200,
+      message: 'API is running successfully',
+    };
   }
 }
